b251a28b9857702f0a296476edaaf8aff7bbd0a2
VerechoTJI <tangjiyi0416@gmail.com>
2026-04-18 09:28:28 +0800

    feat(prototype): set store mode to memory for in-memory data handling

--- prototype/src/main.js
+++ prototype/src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { setMode } from "./lib/store"; //TMP
+setMode("memory"); //TMP
+
 createApp(App).use(router).mount("#app");
